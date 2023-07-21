61afdbf742d7dddcf6b36084a6ff3f568e803cca
yitao.wei <254351821@qq.com>
2023-07-22 00:23:37 +0800

    category edit and delete and importmap add admin.js

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
-    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
+    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: :desc)
 
   end
 
@@ -17,6 +17,36 @@ class Admin::CategoriesController < Admin::BaseController
     else
       @root_categories = Category.roots.order(id: :desc)
       render action: :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+    @root_categories = Category.roots
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(params.require(:category).permit!)
+      flash[:notice] = "修改成功"
+      redirect_to admin_categories_path
+    else
+      flash[:notice] = "修改失败"
+      @root_categories = Category.roots
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+
+    if @category.destroy
+      flash[:notice] = "删除成功"
+      redirect_to admin_categories_path
+    else
+      flash[:notice] = "删除失败"
+      redirect_to admin_categories_path
     end
   end
 end
